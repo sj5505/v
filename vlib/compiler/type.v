336ed178a48b3d956c2898daba00a3d8fde621ef
Luther Wen Xu <lutherchanpublic+git@gmail.com>
2019-12-19 18:38:31 +0800

    vlib/compiler: Add generic types

--- vlib/compiler/type.v
+++ vlib/compiler/type.v
@@ -1,5 +1,12 @@
 module compiler
 
+interface Typer{
+	// Super type is basically types that can be effectively replace this type.
+	// For example a MutableType can be casted to its non-mutable version safely.
+}
+
+struct ToDo {}
+
 enum IntType {
 	i8
 	i16
@@ -18,6 +25,43 @@ enum FloatType {
 	f64
 }
 
-interface Typer{}
+struct VoidType {}
 
-struct ToDo{}
+struct BoolType {}
+
+struct StringType {}
+
+struct OptionalType {
+	underlying Type
+}
+
+struct StructType {
+	property_type map[string]Type
+}
+
+struct FunctionType {
+	parameters  []Type
+	return_type Type
+}
+
+struct ArrayType {
+	element_type Type
+}
+
+struct MapType {
+	key_type   Type
+	value_type Type
+}
+
+struct PointerType {
+	pointed Type
+}
+
+struct MutableType {
+	mutable Type
+}
+
+fn greatest_common_type(left, right Typer) Typer {
+	//TODO Finish type system
+	return ToDo{}
+}
